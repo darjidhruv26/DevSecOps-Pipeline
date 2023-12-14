41ab426088eec23034b185d70fa93ebf27eefb31
Dhruv Darji <90086813+darjidhruv26@users.noreply.github.com>
2023-12-14 22:12:06 +0530

    EKS Install

--- jenkins_terraform/install_jenkins.sh
+++ jenkins_terraform/install_jenkins.sh
@@ -46,3 +46,8 @@ sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
+#EKS install
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+cd /tmp
+sudo mv /tmp/eksctl /bin
+

be1c749e5ec18edfcf13a8c825ad69c54214fa6d
Dhruv Darji <90086813+darjidhruv26@users.noreply.github.com>
2023-12-15 17:37:07 +0530

    Helm Install

--- jenkins_terraform/install_jenkins.sh
+++ jenkins_terraform/install_jenkins.sh
@@ -51,3 +51,7 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 cd /tmp
 sudo mv /tmp/eksctl /bin
 
+#Helm install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
